c86b4406e4aa0f8d3f03f6839c790241623e7e05
canalverdade2-ship-it <canalverdade2@gmail.com>
2026-07-20 15:01:08 -0300

    Corrigir atributos dos runtimes JSX

--- src/types/react-global.d.ts
+++ src/types/react-global.d.ts
@@ -3,23 +3,33 @@ import type {
   ReactNode as ReactNodeType,
 } from 'react';
 
+type GsaReactKey = string | number | bigint;
+
 declare global {
   namespace React {
     type ElementType = ReactElementType;
     type ReactNode = ReactNodeType;
-    type Key = string | number | bigint;
+    type Key = GsaReactKey;
 
     interface Attributes {
       key?: Key | null;
     }
+  }
+}
 
-    namespace JSX {
-      interface IntrinsicAttributes extends Attributes {}
+declare module 'react/jsx-runtime' {
+  namespace JSX {
+    interface IntrinsicAttributes {
+      key?: GsaReactKey | null;
     }
   }
+}
 
+declare module 'react/jsx-dev-runtime' {
   namespace JSX {
-    interface IntrinsicAttributes extends React.Attributes {}
+    interface IntrinsicAttributes {
+      key?: GsaReactKey | null;
+    }
   }
 }
 
