f2e2a38afd9f76dab1fcfddd302ebfd028547945
canalverdade2-ship-it <canalverdade2@gmail.com>
2026-07-20 13:51:11 -0300

    test: executar inspeção do contrato de sessão

--- scripts/gsa-custom-auth-inspection.sql
+++ scripts/gsa-custom-auth-inspection.sql
@@ -1,3 +1,5 @@
-SELECT pg_get_functiondef(
-  'public.gsa_client_session_actor(uuid,text)'::regprocedure
-) AS function_definition;
+SELECT
+  pg_get_functiondef('public.gsa_client_session_actor(uuid,text)'::regprocedure)
+  || E'\n-- RESULT: '
+  || pg_get_function_result('public.gsa_client_session_actor(uuid,text)'::regprocedure)
+  AS function_definition;
