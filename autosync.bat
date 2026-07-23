0f7c3f8b0982dc5c31cd6e6cd3d20c57a8146c23
Associação de Moradores Mil Flores <associacao@milflores.com>
2026-07-23 17:01:16 -0300

    Auto Sync

--- autosync.bat
+++ autosync.bat
@@ -1,11 +1,29 @@
 @echo off
+title GSA Auto Sync
+
+cd /d "C:\Users\Adriano Farias\Downloads\remix-9.10_-grupo-gsa---gestão-de-serviços - Copia (4)"
 
 :loop
-git pull --rebase
-git add .
-git commit -m "Auto Sync"
-git push
 
-timeout /t 1 >nul
+git add .
+
+git diff --cached --quiet
+if errorlevel 1 (
+    echo.
+    echo ======================================
+    echo [%date% %time%] Alteracao detectada...
+    echo ======================================
+
+    git pull --rebase
+
+    git commit -m "Auto Sync"
+
+    git push
+
+    echo.
+    echo Sincronizacao concluida!
+)
+
+timeout /t 2 >nul
 
 goto loop
