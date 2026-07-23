66debe4a096480d023c15533ec7bef1f04e3b4e4
Associação de Moradores Mil Flores <associacao@milflores.com>
2026-07-23 17:35:47 -0300

    Auto Sync

--- autosync.bat
+++ autosync.bat
@@ -2,25 +2,36 @@
 title GSA AUTO SYNC
 color 0A
 
+:: Caminho do projeto
 cd /d "C:\Users\Adriano Farias\Downloads\remix-9.10_-grupo-gsa---gestão-de-serviços - Copia (4)"
 
 :LOOP
 
+echo.
+echo ======================================
+echo %date% %time%
+echo ======================================
+
+:: Descobre automaticamente a branch
 for /f %%i in ('git branch --show-current') do set BRANCH=%%i
 
-git status --porcelain >nul
-
+:: Adiciona alterações
 git add .
 
+:: Commit apenas se houver alterações
 git diff --cached --quiet
 
 if errorlevel 1 (
-    echo Alteracoes encontradas...
     git commit -m "Auto Sync"
-    git pull --rebase origin %BRANCH%
-    git push origin %BRANCH%
 )
 
+:: Atualiza do GitHub
+git pull --rebase origin %BRANCH%
+
+:: Envia alterações
+git push origin %BRANCH%
+
+:: Aguarda 1 segundo
 timeout /t 1 /nobreak >nul
 
 goto LOOP
