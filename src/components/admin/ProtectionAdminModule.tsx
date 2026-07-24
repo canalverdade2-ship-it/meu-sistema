9511c54a860b94d0998b537addc4fc67d7b6baea
canalverdade2-ship-it <canalverdade2@gmail.com>
2026-07-24 13:25:47 -0300

    feat: remover catálogo administrativo do GSA Saúde

--- src/components/admin/ProtectionAdminModule.tsx
+++ src/components/admin/ProtectionAdminModule.tsx
@@ -1,15 +1,10 @@
 import type { ComponentProps } from 'react';
-import { ProtectionAdminModule as LegacyProtectionAdminModule } from './ProtectionAdminModuleLegacy';
-import { ProtectionAdminModule as InsuranceProtectionAdminModule } from './InsuranceProtectionAdminModule';
+import { ProtectionAdminModule as DirectQuoteProtectionAdminModule } from './InsuranceProtectionAdminModule';
 
-type ProtectionAdminModuleProps = ComponentProps<typeof LegacyProtectionAdminModule>;
+type ProtectionAdminModuleProps = ComponentProps<typeof DirectQuoteProtectionAdminModule>;
 
 export function ProtectionAdminModule(props: ProtectionAdminModuleProps) {
-  if (props.domain === 'seguros') {
-    return <InsuranceProtectionAdminModule {...props} domain="seguros" />;
-  }
-
-  return <LegacyProtectionAdminModule {...props} domain="saude" />;
+  return <DirectQuoteProtectionAdminModule {...props} />;
 }
 
 export default ProtectionAdminModule;
