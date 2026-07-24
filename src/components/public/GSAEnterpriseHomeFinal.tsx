50dd40db243b2d6d633158f9d1bdd03c3f4e0521
canalverdade2-ship-it <canalverdade2@gmail.com>
2026-07-24 00:17:32 -0300

    Conecta login de cliente ao benefício Pro

--- src/components/public/GSAEnterpriseHomeFinal.tsx
+++ src/components/public/GSAEnterpriseHomeFinal.tsx
@@ -104,16 +104,11 @@ export function GSAEnterpriseHomeFinal(props: GSAEnterpriseHomeFinalProps) {
         <button type="button" data-dialog-autofocus onClick={dismissIntro} className="mt-10 rounded-full border border-white/25 px-5 py-2 text-sm font-bold text-white hover:bg-white/10 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#d8bd73]">Pular introdução</button>
       </AccessibleDialog>
 
-      <PublicHeader
-        currentPage={props.publicPage}
-        onNavigatePage={props.setPublicPage}
-        onGuestStore={props.onGuestStore}
-        onClientLogin={props.onClientLogin}
-      />
+      <PublicHeader currentPage={props.publicPage} onNavigatePage={props.setPublicPage} onGuestStore={props.onGuestStore} onClientLogin={props.onClientLogin} />
 
       {props.publicPage === 'home' && <PublicHomeLanding reduceMotion={Boolean(reduceMotion)} setPublicPage={props.setPublicPage} onGuestStore={props.onGuestStore} />}
       {props.publicPage === 'services' && <PublicServicesPage audience={props.publicAudience} setAudience={props.setPublicAudience} packages={filteredPackages} publicServices={props.publicServices} onBack={() => props.setPublicPage('home')} onSelect={(item) => { setSelectedPackage(item); props.onServiceDetailChange?.(getServicePackageSlug(item)); }} />}
-      {props.publicPage === 'free-tools' && <FreeToolsExperiencePage onBack={() => props.setPublicPage('home')} onServices={() => props.setPublicPage('services')} />}
+      {props.publicPage === 'free-tools' && <FreeToolsExperiencePage onBack={() => props.setPublicPage('home')} onServices={() => props.setPublicPage('services')} onClientLogin={props.onClientLogin} />}
       {props.publicPage === 'partners' && <PartnersPage selectedSlug={props.initialPartnerSlug} onSelectPartner={(slug) => props.onPartnerDetailChange?.(slug)} onBack={() => props.setPublicPage('home')} />}
 
       <PublicFooter setPublicPage={props.setPublicPage} onGuestStore={props.onGuestStore} onAdminLogin={props.onAdminLogin} onPrivacy={() => setPrivacyOpen(true)} />
