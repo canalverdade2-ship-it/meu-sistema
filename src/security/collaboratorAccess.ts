ba753070ce81516214ab8e5439db68096b64472f
canalverdade2-ship-it <canalverdade2@gmail.com>
2026-07-24 18:43:48 -0300

    Adicionar prioridade de acesso aos avisos e campanhas

--- src/security/collaboratorAccess.ts
+++ src/security/collaboratorAccess.ts
@@ -41,6 +41,7 @@ export function defaultAdminPath(adminType: AdminType | undefined, modules: stri
     'fornecedores',
     'parceiros',
     'trabalhe-conosco',
+    'avisos-campanhas',
     'anuncios',
     'operacoes',
     'financeiro',
