308fdd053228c15df109c804732c0129fad7c87a
canalverdade2-ship-it <canalverdade2@gmail.com>
2026-07-23 21:30:44 -0300

    Inverte texto e arte na galeria de marca

--- src/components/public/BrandExamplesDialog.tsx
+++ src/components/public/BrandExamplesDialog.tsx
@@ -1,2 +1,32 @@
-export { BrandExamplesDialog } from './BrandPortfolioInteractiveDialog';
+import type { ComponentProps } from 'react';
+import { BrandExamplesDialog as BrandPortfolioInteractiveDialog } from './BrandPortfolioInteractiveDialog';
+
 export type { BrandExampleCategory } from './BrandPortfolioInteractiveDialog';
+
+type BrandExamplesDialogProps = ComponentProps<typeof BrandPortfolioInteractiveDialog>;
+
+export function BrandExamplesDialog(props: BrandExamplesDialogProps) {
+  return (
+    <>
+      <style>{`
+        [role="dialog"][aria-label^="Galeria de referências"] > div > div:nth-child(2) > div:first-child {
+          display: flex;
+          flex-direction: column;
+        }
+
+        [role="dialog"][aria-label^="Galeria de referências"] > div > div:nth-child(2) > div:first-child > div:first-child {
+          order: 2;
+          margin-top: 1.5rem;
+        }
+
+        [role="dialog"][aria-label^="Galeria de referências"] > div > div:nth-child(2) > div:first-child > div:nth-child(2) {
+          order: 1;
+          margin-top: 0 !important;
+          padding-top: 0 !important;
+          border-top-width: 0 !important;
+        }
+      `}</style>
+      <BrandPortfolioInteractiveDialog {...props} />
+    </>
+  );
+}
