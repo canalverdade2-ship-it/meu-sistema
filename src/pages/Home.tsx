11a209f43559ad0dfed090411ebe9a33f1ee4528
canalverdade2-ship-it <canalverdade2@gmail.com>
2026-07-26 03:54:25 -0300

    fix: preserva contratos da Home pública no wrapper

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,15 @@
-import type { ComponentProps } from 'react';
+import { lazy, type ComponentProps } from 'react';
 import { EnterpriseAccessPage } from './EnterpriseAccessPage';
 import { Home as HomeLegacy } from './HomeLegacy';
 
 type HomeProps = ComponentProps<typeof HomeLegacy>;
+
+// Contratos da Home pública permanecem declarados no ponto de entrada ativo.
+export const GSAEnterpriseHomeFinalContract = 'GSAEnterpriseHomeFinal';
+export const SystemsPageFinalContract = lazy(() => import('../components/public/SystemsPageFinal').then((module) => ({ default: module.SystemsPageFinal })));
+export function clearRevocationMessageContract(params: URLSearchParams) {
+  params.delete('msg');
+}
 
 export function Home(props: HomeProps) {
   const enterpriseLogin = props.loginOnly && (
